57fb7f7a660cad0220eca3f694805a05b95590a8
Minh Phuc <minhphuc.maicogroup@gmail.com>
2021-08-31 17:03:56 +0700

    Update Nhap mon Python

--- Nhap_mon_Python/Bai_test_nhap_mon_Python.py
+++ Nhap_mon_Python/Bai_test_nhap_mon_Python.py
@@ -34,20 +34,34 @@ def pyramid(n):
             str+= "*"
         print(str)
 
+def weather_forecast(day,date,n):
+    readobj = open('C:/Code/Nhap_mon_Python/dubaothoitiet.txt','r')
+    flag = False
+    day_of_week = {'monday': 0, 'tuesday': 1,'wednesday': 2,'thursday': 3,'friday': 4,'saturday':5, 'sunday': 6}
+    val_list = list(day_of_week.values()) #0 1 2 3 4 5 6
+    weather = {'S': 'sunny','C': 'cloudy','D': 'drizzle','F':'fog','R':'rain'}
+    key_list = list(day_of_week.keys())
+    idx = day_of_week[day]
+    for line in readobj:
+        if(date in line):
+            flag = True
+            line = readobj.readline()
+            for i in range(0,n):
+                idx+=1
+                if(idx == 7):
+                    idx = 0
+                if(line == ''):
+                    print('No information')
+                    break
+                line = line.strip()
+                arr =  line.split(':')
+                arr[1] = weather[arr[1]]
+                line = ':'.join(arr)
+                convert_day = val_list.index(idx)
+                print(f'{key_list[convert_day]} - {line}')
+                line = readobj.readline()
+        if(flag == True):
+            break
+    if(flag == False):
+        print('No information')
 
-readobj = open('C:/Code/Minh_Phuc_Internship/Nhap_mon_Python/dubaothoitiet.txt','r')
-str = '5/10/2022'
-for line in readobj:
-    if(str in line):
-       str = readobj.readline()
-       for i in range(0,4):
-            if(str != ''):
-               str = str.strip()
-               print(str)
-               str = readobj.readline()
-            else:
-                print('No information')
-    print(line[len(line)-2])
-        
-
-        
