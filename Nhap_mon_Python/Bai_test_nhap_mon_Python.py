374c11df5de9f68db13546ab7c983f7773f51cc3
Minh Phuc <minhphuc.maicogroup@gmail.com>
2021-09-02 08:58:10 +0700

    Hoan thien bai test Python co ban

--- Nhap_mon_Python/Bai_test_nhap_mon_Python.py
+++ Nhap_mon_Python/Bai_test_nhap_mon_Python.py
@@ -29,41 +29,41 @@ def lowestCommonMultiple(first_number,second_number):
 
 
 def drawPyramid(n):
-    numberOfStars = 1+ 2*(n-1)
+    number_of_stars = 1+ 2*(n-1)
     for i in range(0,n):
         str = "" 
         for j in range(0,i):
             str += " " 
-        for k in range(i,numberOfStars-i):
+        for k in range(i,number_of_stars-i):
             str+= "*"
         print(str)
 
 
 def readSymbol():
-    readFile = open("Nhap_mon_Python/kyhieu.txt","r")
-    dicSym = dict()
-    for line in readFile:
+    read_file = open("Nhap_mon_Python/kyhieu.txt","r")
+    dic_symbol = dict()
+    for line in read_file:
         if(line == ""):
-            readFile.close()
-            return dicSym
+            read_file.close()
+            return dic_symbol
         List = line.split(":")
         List[0] = List[0].strip()
         List[1] = List[1].strip()
-        dicSym[List[0]] = List[1]
-    return dicSym
+        dic_symbol[List[0]] = List[1]
+    return dic_symbol
 
 
 def weatherForecast(day,date,next_n_days):
-    readFile = open("Nhap_mon_Python/dubaothoitiet.txt","r") 
+    read_file = open("Nhap_mon_Python/dubaothoitiet.txt","r") 
     flag = False 
     DAY_OF_WEEK = ("monday", "tuesday","wednesday","thursday","friday","saturday", "sunday")
     weather = readSymbol()
     idx = DAY_OF_WEEK.index(day)
 
-    for line in readFile:
+    for line in read_file:
         if(date in line):
             flag = True 
-            line = readFile.readline()
+            line = read_file.readline()
             for i in range(0,next_n_days):
                 idx+=1
                 if(idx == 7):
@@ -76,15 +76,15 @@ def weatherForecast(day,date,next_n_days):
                 #VD: line = 11/9/2022:S => arr= ['11/9/2022','S']
                 arr[1] = weather[arr[1]]
                 line = ':'.join(arr)
-                convertDay = DAY_OF_WEEK[idx]
-                print(f'{convertDay} - {line}')
-                line = readFile.readline()
+                convert_day = DAY_OF_WEEK[idx]
+                print(f'{convert_day} - {line}')
+                line = read_file.readline()
         if(flag == True):
             break
 
     if(flag == False): #neu du lieu yeu cau khong xuat hien trong file
         print('No information')
-    readFile.close()
+    read_file.close()
 
 
 if __name__ == "__main__":
