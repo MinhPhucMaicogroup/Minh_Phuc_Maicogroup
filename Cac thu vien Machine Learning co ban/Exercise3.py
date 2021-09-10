8c68d407d3e96acb051d22d37d08f34067a2eb0c
Minh Phuc <minhphuc.maicogroup@gmail.com>
2021-09-10 13:35:36 +0700

    Update ML Lib

--- Cac thu vien Machine Learning co ban/Exercise3.py
+++ Cac thu vien Machine Learning co ban/Exercise3.py
@@ -5,6 +5,7 @@ data_set = pd.read_csv("C:/Code/Cac thu vien Machine Learning co ban/dataset.csv
 active = dict()
 print('First 50 line:')
 print(data_set.head(50))
+print("")
 print(data_set[["Date", "Calories (kcal)", "Distance (m)"]])
 
 for index, value in data_set["Active Minutes"].items():
