454d22910092c4a5fb5a4a52da73eb0a07fd1142
Minh Phuc <minhphuc.maicogroup@gmail.com>
2021-09-10 10:29:51 +0700

    Update ML Lib

--- Cac thu vien Machine Learning co ban/Exercise3.py
+++ Cac thu vien Machine Learning co ban/Exercise3.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-data_set = pd.read_csv("C:/Code/Cac thu vien Machine Learning co ban/dataset.csv")
+data_set = pd.read_csv("dataset.csv")
 active = dict()
 print('First 50 line:')
 print(data_set.head(50))
