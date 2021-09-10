6bf6bb9fd6640e266829109ba80ba03e6fff96b0
Minh Phuc <minhphuc.maicogroup@gmail.com>
2021-09-10 13:33:34 +0700

    Update ML Lib

--- Cac thu vien Machine Learning co ban/Exercise3.py
+++ Cac thu vien Machine Learning co ban/Exercise3.py
@@ -1,20 +1,11 @@
 import numpy as np
 import pandas as pd
 
-data_set = pd.read_csv("dataset.csv")
+data_set = pd.read_csv("C:/Code/Cac thu vien Machine Learning co ban/dataset.csv")
 active = dict()
 print('First 50 line:')
 print(data_set.head(50))
-print("")
-print("Date:")
-print(data_set["Date"])
-print("")
-print("Calories (kcal):")
-print(data_set["Calories (kcal)"])
-print("")
-print("Distance (m):")
-print(data_set["Distance (m)"])
-print("")
+print(data_set[["Date", "Calories (kcal)", "Distance (m)"]])
 
 for index, value in data_set["Active Minutes"].items():
     if value > 100:
