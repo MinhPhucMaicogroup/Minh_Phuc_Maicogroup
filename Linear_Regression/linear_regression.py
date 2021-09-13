d4fc726ebb24e0c288ab4f7f053cc7bf5e6f4579
Minh Phuc <minhphuc.maicogroup@gmail.com>
2021-09-13 15:43:39 +0700

    Update Linear Regression

--- Linear_Regression/linear_regression.py
+++ Linear_Regression/linear_regression.py
@@ -12,7 +12,7 @@ def concatenate(x):
 data_set = pd.read_csv("Linear_Regression/linear_regression.csv")
 training_set = data_set.head(160)
 testing_set = data_set.tail(40)
-
+# Finding coefficients
 x_bar = training_set['x'].to_numpy().reshape((160, 1))
 xbar = concatenate(x_bar)
 y_training = training_set['y'].to_numpy().reshape((160, 1))
@@ -21,7 +21,7 @@ A = np.dot(x_tranpose, xbar)
 b = np.dot(x_tranpose, y_training)
 A_inverse = np.linalg.pinv(A)
 W = np.dot(A_inverse, b)
-
+# Find minium Error 
 x_test = testing_set['x'].to_numpy().reshape((40, 1))
 x_test = concatenate(x_test)
 y_hat = np.dot(x_test, W)
@@ -30,7 +30,7 @@ error = round((np.linalg.norm(y_test - y_hat)**2)/2, 5)
 x_axis = np.linspace(-3, 7)
 y_axis = W[0][0]*x_axis + W[1][0]
 print("Error: ", error)
-
+#Visualize the Fitting Line
 plt.plot(x_axis, y_axis, label=f"y = {round(W[0][0], 1)}x + {round(W[1][0], 1)}")
 plt.scatter(data_set['x'], data_set['y'], c='r')
 plt.xlabel("X axis")
