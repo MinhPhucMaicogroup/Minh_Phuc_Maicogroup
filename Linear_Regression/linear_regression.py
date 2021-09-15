7a23efc90c131df95cc9eb1af28a3313fd6b0f1f
Minh Phuc <minhphuc.maicogroup@gmail.com>
2021-09-15 07:31:05 +0700

    Fix theo gop y Hai Dang

--- Linear_Regression/linear_regression.py
+++ Linear_Regression/linear_regression.py
@@ -16,25 +16,27 @@ def append_one(x):
     x = np.concatenate((x, one), axis=1)
     return x
 
-def to_2dnumpy(series):
-    arr = series.to_numpy()
-    arr = arr.reshape(arr.shape[0], 1)
-    return arr
+
+def to_matrix(series):
+    matrx = series.to_numpy()
+    matrx = matrx.reshape(matrx.shape[0], 1)
+    return matrx
+
 
 data_set = pd.read_csv("Linear_Regression/linear_regression.csv")
 train_set = data_set.head(160)
 test_set = data_set.tail(40)
 compute_cost = lambda output, model: ((np.linalg.norm(output - model))**2)/2
 
-x_train = to_2dnumpy(train_set["x"])
+x_train = to_matrix(train_set["x"])
 x_train = append_one(x_train)
-y_train = to_2dnumpy(train_set["y"])
+y_train = to_matrix(train_set["y"])
 coeffs = coefficient(x_train, y_train)
 
-x_output = to_2dnumpy(test_set["x"])
+x_output = to_matrix(test_set["x"])
 x_output = append_one(x_output)
 model = np.dot(x_output, coeffs)
-y_output = to_2dnumpy(test_set["y"])
+y_output = to_matrix(test_set["y"])
 cost = compute_cost(y_output, model)
 print(f"Error term: {cost:.3f}")
 
